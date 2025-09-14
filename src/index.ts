0d1410d6c02f558382dc29fc28395efce8bede3b
muhaigor <mukhinighor@gmail.com>
2025-09-14 09:24:41 +0200

    3tg

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import { development, production } from './core';
 const token = '7325353221:AAEta0uc1hlRSOEDiIsvYkBwbgza7Y-oPlM';
 const BOT_TOKEN = token || process.env.BOT_TOKEN || '';
-const ENVIRONMENT = token || process.env.NODE_ENV || '';
+const ENVIRONMENT = process.env.NODE_ENV || '';
 
 const bot = new Telegraf(BOT_TOKEN);
 
